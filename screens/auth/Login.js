33e2cb463650c8d483f520f9a394485963b9f17d
AKSHAY KUMAR SINGH <akshaymemail@gmail.com>
2022-03-14 12:25:54 +0530

    add login page

--- screens/auth/Login.js
+++ screens/auth/Login.js
@@ -1,18 +1,103 @@
-import { StyleSheet, Text, View } from 'react-native'
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput,
+  Platform,
+  KeyboardAvoidingView,
+} from 'react-native'
 import React from 'react'
 
 export default function Login() {
   return (
     <View style={styles.screen}>
-      <Text>Login Screen</Text>
+      <LoginTitle />
+      <Image
+        style={styles.image}
+        source={require('../../assets/images/login.jpg')}
+      />
+      <BottomSheet />
     </View>
+  )
+}
+
+const LoginTitle = () => {
+  return (
+    <View style={styles.titleContainer}>
+      <Text style={{ ...styles.loginTitleText, color: '#000000' }}>
+        Uber Eats
+      </Text>
+      <Text style={{ ...styles.loginTitleText, color: 'green' }}>Clone</Text>
+    </View>
+  )
+}
+
+const BottomSheet = () => {
+  return (
+    <KeyboardAvoidingView
+      style={styles.bottomSheet}
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+    >
+      <Text style={styles.bottomSheetText}>
+        Use your Uber account to get started
+      </Text>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="akshaymemail@gmail.com"
+          keyboardType="email-address"
+        />
+      </View>
+    </KeyboardAvoidingView>
   )
 }
 
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+  },
+  titleContainer: {
+    position: 'absolute',
+    zIndex: 1,
+    top: '10%',
+    left: '5%',
+  },
+  loginTitleText: {
+    fontSize: 40,
+    fontWeight: 'bold',
+  },
+
+  bottomSheet: {
+    position: 'absolute',
+    bottom: 0,
+    backgroundColor: '#ffffff',
+    width: '100%',
+    height: '18%',
+    paddingVertical: 20,
+    paddingHorizontal: 25,
+  },
+  bottomSheetText: {
+    fontSize: 20,
+    fontWeight: '600',
+    color: '#424242',
+  },
+  inputContainer: {
+    flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
+  },
+  input: {
+    marginTop: 10,
+    backgroundColor: '#f2f2f2',
+    borderColor: '#cccccc',
+    borderWidth: 1,
+    borderRadius: 4,
+    paddingHorizontal: 10,
+    paddingVertical: 8,
+    fontSize: 20,
   },
 })
